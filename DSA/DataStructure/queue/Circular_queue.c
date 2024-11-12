e55204a2fc293e701d5537f0252594ca3b7177f1
ank501 <ankitsyadav246@gmail.com>
2024-11-12 07:51:46 +0530

    enqueu function in circular queue is completed

--- DSA/DataStructure/queue/Circular_queue.c
+++ DSA/DataStructure/queue/Circular_queue.c
@@ -6,11 +6,19 @@ int rear = -1;
 
 
 void Enqueu(int data){
-    if(front==0 && rear ==size-1 ){
+    if(front==0 && rear ==size-1 || front == rear+1 ){
        printf("\n queue is full");
     }
-    else {
-        circular_queue[rear] = data;
+    if(rear!=size-1){
+        if(front ==-1){
+            front=0; 
+        }
+        rear++;
+        circular_queue[rear]=data;
+    }
+    if(rear == size -1 && front !=0 ){
+          rear=0;
+          circular_queue[rear] = data;
     }
 }
 
