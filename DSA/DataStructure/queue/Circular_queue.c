b6f6ca5c42df8fb31cfa8398f11dd7408ec19618
ank501 <ankitsyadav246@gmail.com>
2024-11-12 07:44:50 +0530

    circular queue enque function

--- DSA/DataStructure/queue/Circular_queue.c
+++ DSA/DataStructure/queue/Circular_queue.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#define size 6
+int circular_queue[size];
+int front = -1;
+int rear = -1;
+
+
+void Enqueu(int data){
+    if(front==0 && rear ==size-1 ){
+       printf("\n queue is full");
+    }
+    else {
+        circular_queue[rear] = data;
+    }
+}
 
 int main (){
     return 0;
