1354ffd5b46cf85bb2a8604342233f431ed6d28d
ank501 <ankitsyadav246@gmail.com>
2024-11-14 07:02:37 +0530

    insertion sort is completed and working fine

--- DSA/Algorithm/sorting/Insertion_sort.c
+++ DSA/Algorithm/sorting/Insertion_sort.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int arr[]={8,5,3,6,8};
+int arr[]={8,5,3,6,1};
 
 int main(){
-    for(int i=1;i<5;i++){
+    for(int i=0;i<5;i++){
         int temp =arr[i];
         int j = i-1;
         
         while(j>=0 && arr[j]>temp){
-            arr[i+1]=arr[j];
+            arr[j+1]=arr[j];
             j--;
         }
         arr[j+1]=temp;
