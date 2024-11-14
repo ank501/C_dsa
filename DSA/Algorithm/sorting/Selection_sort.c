f8c7e1661534a6f9d3828a4313b1bac456d5845d
ank501 <ankitsyadav246@gmail.com>
2024-11-14 07:16:47 +0530

    Selection sort by selection based on index

--- DSA/Algorithm/sorting/Selection_sort.c
+++ DSA/Algorithm/sorting/Selection_sort.c
@@ -1,5 +1,19 @@
-#include <stdint.h>
+#include <stdio.h>
+int arr[]={8,5,3,6,1};
 // Selection sort ;
+
 int main (){
+    for(int i=0;i<5;i++){
+      for(int j =i+1 ;j<5;j++){
+        if(arr[i]>arr[j]){
+          int temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+    for(int i=0;i<5;i++){
+        printf("%d ",arr[i]);
+    }
     return 0;
 }
