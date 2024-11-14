7a0f65adb3d655e1a68aa7f2f7b968a737b04c7b
ank501 <ankitsyadav246@gmail.com>
2024-11-14 23:34:32 +0530

    divide function is completed using recursion,,, merge function is giving error

--- DSA/Algorithm/sorting/Merge_sort.c
+++ DSA/Algorithm/sorting/Merge_sort.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
-void merge(){
 
+void merge(arr, start,end,mid){
+  int left_arr_size = mid-start;
+  int right_arr_size = end-mid;
+
+  int left_arr[left_arr_size] ;
+  int right_arr[right_arr_size];
+  
+  int i;
+  int j;
+  for(i=0;i<left_arr_size;i++){
+      left_arr[i]=arr[start+i];
+  }
+  for(j=0;j<right_arr_size;j++){
+    right_arr[j]=arr[mid+i+1];
+  }
 }
 
 void divide(int arr[],int start,int end){
@@ -8,6 +22,7 @@ void divide(int arr[],int start,int end){
   if(start<end){
     divide(arr , start ,mid);
     divide(arr , mid+1 ,end);
+    merge(arr, start,end,mid);
   }
 }
 int main(){
