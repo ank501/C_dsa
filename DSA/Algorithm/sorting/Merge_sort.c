17c27efef492a996e9b232360eeb51f246001a2a
ank501 <ankitsyadav246@gmail.com>
2024-11-14 21:01:45 +0530

    divide function created

--- DSA/Algorithm/sorting/Merge_sort.c
+++ DSA/Algorithm/sorting/Merge_sort.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+void merge(){
 
+}
+
+void divide(int arr[],int start,int end){
+  int mid=(start+end)/2;
+  if(start<end){
+    divide(arr , start ,mid);
+    divide(arr , mid+1 ,end);
+  }
+}
 int main(){
-    
+ return 0;
 }
