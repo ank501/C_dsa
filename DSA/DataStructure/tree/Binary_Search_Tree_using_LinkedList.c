6c0c796c63e9792b8b5222fc7196a0f730552e84
ank501 <ankitsyadav246@gmail.com>
2024-11-25 20:56:01 +0530

    traversal inoder preorder and post order completed

--- DSA/DataStructure/tree/Binary_Search_Tree_using_LinkedList.c
+++ DSA/DataStructure/tree/Binary_Search_Tree_using_LinkedList.c
@@ -58,25 +58,57 @@ void inOrder(struct Node * temp){
     inOrder(temp->right);
    }
 }
-
+void preOrder(struct Node * temp){
+    if(temp!=NULL){
+        printf("%d ",temp->data);
+        preOrder(temp->left);
+        preOrder(temp->right);
+    }
+}
+void postOrder(struct Node * temp){
+    if(temp!=NULL){
+        preOrder(temp->left);
+        preOrder(temp->right);
+        printf("%d ",temp->data);
+    }
+}
 
 int main(){
    int choice ,data;
    while (1)
    {
-    printf("\n select option \n 1 for enter data \n 2 for traverse \n 3 for exit");
+    printf("\n Select option to perform operation \n\n 1.Insert data \n 2. Traverse (InOrder)\n 3.Traverse(PreOrder)\n 4.Traverse(PostOrder)\n 5.Traversal (In all order) \n 6.for exit \n \n");
+    printf("\n Enter Your choice");
     scanf("%d",&choice);
+ 
     switch (choice)
     {
-    case 1: printf("Enter data");
+    case 1: printf("Enter data \n ");
             scanf("%d",&data);
             insert(data);
             break;
-    case 2: inOrder(root);
+    case 2: printf("\n In Order Traversal : ");
+            inOrder(root);
             break;
-    case 3: printf("Exiting program.\n");
+    case 3: printf("\n Pre Order Traversal : ");
+            preOrder(root);
+            break;
+    case 4: printf("\n Post Order Traversal : ");
+            postOrder(root);
+            break;
+    case 5: printf("\n In Order Traversal : ");
+            inOrder(root); 
+            printf("\n");
+            printf("\n Pre Order Traversal : ");
+            preOrder(root);  
+            printf("\n");
+            printf("\n Post Order Traversal : ");
+            postOrder(root); 
+            printf("\n");
+
+    case 6: printf("Exiting program.\n");
             exit(0); // Exit the program
-            break;
+    
     default: printf("Invalid option. Please try again.\n");
   
     }
