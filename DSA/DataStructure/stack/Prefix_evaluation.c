a240e4a1722974d514a1b1d1dd9857e111a9a872
ank501 <ankitsyadav246@gmail.com>
2024-11-25 07:52:41 +0530

    prefix evaluation code is not completed

---
+++ DSA/DataStructure/stack/Prefix_evaluation.c
@@ -0,0 +1,59 @@
+/* Program for evaluating postfix expression */
+
+#include <stdio.h>
+#include <string.h>
+#define SIZE 20
+
+int stack[SIZE] ;
+int top=-1;
+
+void push(int operand){
+    if(top==SIZE){
+        printf("\n Stack overflow");
+    }
+    else{
+        top++;
+        stack[top]=operand;
+    }
+}
+void pop(){
+    if(top==-1){
+        printf("\n Stack underflow");
+    }
+    else{
+        top--;
+    }
+}
+
+
+int main()
+{   int i ,op1,op2;
+    char op,preexp[30];
+    printf("\n enter a postfix exp");
+    scanf("%s",preexp);
+    for(i=0;i<strlen(preexp);i++){
+        if(preexp[i]>='0' && preexp[i]<='9'){
+            push(preexp[i]-'0');
+        }
+        else{
+            op1=stack[top];
+            pop();
+            op2=stack[top];
+            pop();
+            
+            char res;
+            switch(preexp[i]){
+                case '+' : res=op2+op1;break;
+                case '-' : res=op2-op1;break;
+                case '*' : res=op2*op1;break;
+                case '%' : res=op2%op1;break;
+                case '/' : res=op2/op1;break;
+
+            }
+            push(res);
+        }
+    }
+    printf("%d",stack[top]);
+    return 0;
+}
+
