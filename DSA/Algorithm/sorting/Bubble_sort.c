c145344979cd3aa45b5205cf105a59c2c2fd15bb
ank501 <ankitsyadav246@gmail.com>
2024-11-30 20:02:25 +0530

    optimised the inner loop for unneccesary iteration

--- DSA/Algorithm/sorting/Bubble_sort.c
+++ DSA/Algorithm/sorting/Bubble_sort.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <time.h>
 #define size 25
 
 // int array [size] = {10,6,3,4,2};
     int arr [25] = {10,82,543,4,17,3,79,33,18,42,7,3,7,90,43,45,245,24,64,13,67,22,535,6554,342} ;
 
 
-int Bubble_sort( int arr[size] ){
+void Bubble_sort( int arr[size] ){
  // outer loop for iterating over whole array for repeating task of comparesion ;
   for(int i=0;i<size;i++){
 // inner loop for iterating over the larray and compare adjacent element for checking and swapping the large element amongst two adjacent element;
-    for(int j=0;j<size-1;j++){
+    for(int j=0;j<size-i-1;j++){
 // swapping performed ;
       if(arr[j]>arr[j+1]){
           int temp = arr[j];
@@ -21,12 +22,14 @@ int Bubble_sort( int arr[size] ){
 }
 
 int main(){
+ 
     // sort function is invoked passing an unsorted array as a argument;
     Bubble_sort(arr);
     // loop for printing the sorted elements of the array;
     for(int i=0;i<size;i++){
         printf("%d ",arr[i]);
     }
+
     return 0;
 }
 
